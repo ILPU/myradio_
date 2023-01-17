c4f567f224e9aa064ecdf3604cebe4a3483f8255
ILPU <kolochkov.andrew@gmail.com>
2023-01-17 14:22:35 +0300

    Add files via upload

---
+++ res/resource.h
@@ -0,0 +1,25 @@
+#ifndef IDC_STATIC
+#define IDC_STATIC (-1)
+#endif
+
+#define IDB_BITMAP1                             100
+#define IDD_DIALOG_CONFIG_CONNECT               102
+#define IDD_DIALOG_CONFIG_OUTPUT                104
+#define IDD_DIALOG_UPDATE_BASE                  106
+#define IDC_CONFIG_CON_PROXY_ON                 40000
+#define IDC_STATIC_FIND_RADIO                   40000
+#define IDC_CONFIG_CON_PROXY_TYPE               40001
+#define IDC_STATIC_RECV_MB                      40001
+#define IDC_CONFIG_CON_PROXY_SERVER             40002
+#define IDC_STATIC_PRORESS_2                    40002
+#define IDC_CONFIG_CON_PROXY_PORT               40003
+#define IDC_STATIC_PRORESS                      40003
+#define IDC_CONFIG_CON_PROXY_PASS               40004
+#define IDC_UPDATE_BASE_BTN_START               40004
+#define IDC_CONFIG_CON_PROXY_AVTORIZ            40005
+#define IDC_UPDATE_BASE_BTN_ABORT               40005
+#define IDC_CONFIG_CON_PROXY_LOGIN              40006
+#define IDC_STATIC_BASE_CREATE                  40007
+#define IDC_STATIC_BASE_MODIF                   40008
+#define IDC_STATIC_BASE_COUNT                   40009
+#define IDC_STATIC_BASE_VER                     40010
